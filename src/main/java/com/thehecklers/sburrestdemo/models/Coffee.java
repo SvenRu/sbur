835cec82bdbb6034f275ac045ab8e15c3845acea
SvenRu <sven.rullmann@gmail.com>
2021-08-29 00:30:19 +0200

    init

---
+++ src/main/java/com/thehecklers/sburrestdemo/models/Coffee.java
@@ -0,0 +1,24 @@
+package com.thehecklers.sburrestdemo.models;
+
+public class Coffee {
+
+    private final String id;
+    private String name;
+
+    public Coffee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
